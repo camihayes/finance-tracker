b36f81443e2b81b19bcc77849abf53fd5642a839
camihayes <camihayes72@gmail.com>
2016-01-07 23:29:04 +0000

    created methods to restrict adding more than 10 stocks or dup stocks.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -44,7 +44,7 @@ class UserStocksController < ApplicationController
     respond_to do |format|
       if @user_stock.save
         format.html { redirect_to my_portfolio_path,
-          notice: "Stock #{@user_stock.stock.ticker} stock was successfully added"}
+          notice: "Stock #{@user_stock.stock.ticker} was successfully added"}
         format.json { render :show, status: :created, location: @user_stock }
       else
         format.html { render :new }
